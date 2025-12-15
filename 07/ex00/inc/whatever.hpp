6f4f7c47e95fd0ae25d4483dc7f2e112f6da6d1e
moist-bread <raquelgo2work@gmail.com>
2025-12-15 17:23:27 +0000

    ...

---
+++ 07/ex00/inc/whatever.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+//-‵,┊ needed libs by class
+#include <iostream>
+
+//-‵,┊ color defines
+
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+
+#define URED "\e[4;31m"
+#define UYEL "\e[4;33m"
+#define UBLU "\e[4;34m"
+#define UMAG "\e[4;35m"
+#define UCYN "\e[4;36m"
+
+#define DEF "\e[0m"
+
+// -->┊( WHATEVER )┊.´-★☆★
+
