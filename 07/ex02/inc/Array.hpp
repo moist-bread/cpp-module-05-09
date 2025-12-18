7c81e6577b73b99177c44fb0b5570c75c680b955
moist-bread <raquelgo2work@gmail.com>
2025-12-18 11:46:34 +0000

    ../..

--- 07/ex02/inc/Array.hpp
+++ 07/ex02/inc/Array.hpp
@@ -3,20 +3,22 @@
 //-‵,┊ needed libs by class
 #include <iostream>
 
-//-‵,┊ color defines
-
-#define GRN "\e[0;32m"
-#define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-
-#define URED "\e[4;31m"
-#define UYEL "\e[4;33m"
-#define UBLU "\e[4;34m"
-#define UMAG "\e[4;35m"
-#define UCYN "\e[4;36m"
-
-#define DEF "\e[0m"
-
 // -->┊( ARRAY )┊.´-★☆★
+
+template <typename T>
+class Array
+{
+public:
+	Array (void); // default constructor
+	Array (unsigned int n);
+	Array (Array const &source); // copy constructor
+	~Array (void); // destructor
+	Array &operator=(Array const &source); // copy assignment operator overload
+	T &operator[](int idx); // subscript operator
+
+	unsigned int size (void) const;
+
+private:
+	T _array;
+	unsigned int _size;
+};
--- 07/ex02/src/main.cpp
+++ 07/ex02/src/main.cpp
@@ -1,12 +1,61 @@
 #include "../inc/Array.hpp"
+#include "../inc/Array.tpp"
 #include <stdio.h>
 #include <string.h>
+
+#define MAX_VAL 750
 
 int main()
 {
 	std::cout << std::endl;
 	std::cout << CYN ">>┈┈┈>   ARRAY 🧢👣";
 	std::cout << DEF << std::endl << std::endl;
+
+	/* Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror; */
 
 	return (0);
 }
