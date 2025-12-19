4497bb2dec2fb171af886a30c09373b0e6f519a5
moist-bread <raquelgo2work@gmail.com>
2025-12-19 10:48:43 +0000

    setup for new module

---
+++ 08/ex00/inc/easyfind.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+//-‵,┊ needed libs by class
+#include <iostream>
+#include <typeinfo>
+#include <stdexcept>
+
+//-‵,┊ color defines
+
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+
+#define URED "\e[4;31m"
+#define UYEL "\e[4;33m"
+#define UBLU "\e[4;34m"
+#define UMAG "\e[4;35m"
+#define UCYN "\e[4;36m"
+
+#define DEF "\e[0m"
+
+// -->┊( easyfind )┊.´-★☆★
+
+template <typename T>
+// void 
---
+++ 08/ex00/src/main.cpp
@@ -0,0 +1,9 @@
+#include "../inc/easyfind.hpp"
+
+int main()
+{
+	std::cout << std::endl;
+	std::cout << CYN ">>┈┈┈>   EASY FIND 🔎🏴‍☠️";
+
+	return (0);
+}
